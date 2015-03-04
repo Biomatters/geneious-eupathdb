ca6479024556089004e813fe043e524c78e1516c
nikhilt <nikhilt@cybage.com>
2015-03-04 16:18:11 +0530

    CYB-19 Convert separate PlasmoDB and PiroplasmaDB plugins into a single plugin
    	- Developed initial JUnit test case

---
+++ test/com/biomatters/plugins/eupathdb/utils/JSONParserTest.java
@@ -0,0 +1,92 @@
+package com.biomatters.plugins.eupathdb.utils;
+
+import static org.junit.Assert.assertNotNull;
+import static com.biomatters.test.CommonTestCase.getTestDataFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.biomatters.plugins.eupathdb.utils.JSONParser;
+
+/**
+ * The class <code>JSONParserTest</code> contains tests for the class
+ * <code>{@link JSONParser}</code>.
+ * 
+ * @author cybage
+ * @version $Revision: 1.0 $
+ */
+public class JSONParserTest {
+	/**
+	 * Run the List<Map<String, String>> parseJSON(String) method test.
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	@Test
+	public void testParseJSON() throws Exception {
+		FileReader reader = null;
+		BufferedReader bufferedReader = null;
+		String inputLine;
+		StringBuilder jsonString = new StringBuilder();
+		try {
+			reader = new FileReader(getTestDataFile(getClass(),
+					"JSONResponseString.txt"));
+			bufferedReader = new BufferedReader(reader);
+
+			while ((inputLine = bufferedReader.readLine()) != null) {
+				jsonString.append(inputLine);
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+			if (bufferedReader != null) {
+				bufferedReader.close();
+			}
+		}
+		List<Map<String, String>> result = JSONParser.parse(jsonString
+				.toString());
+		assertNotNull(result);
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 * 
+	 * @throws Exception
+	 *             if the initialization fails for some reason
+	 * 
+	 */
+	@Before
+	public void setUp() throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 * 
+	 * @throws Exception
+	 *             if the clean-up fails for some reason
+	 * 
+	 */
+	@After
+	public void tearDown() throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 * 
+	 * @param args
+	 *            the command line arguments
+	 * 
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(JSONParserTest.class);
+	}
+}
