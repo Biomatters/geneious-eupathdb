bea14cb876a8cda2b02ee1256a9c968c04fba881
nikhilt <nikhilt@cybage.com>
2015-03-10 18:44:34 +0530

    CYB-25 Add tests to the EuPathDB plugin
    	- Added json parser test cases for error response and broken json.
    	- Updated json parser test case to verify on response parsed object.
    	- Implemented tearDown and added read method for code reusability.
    	- Added test data for json parser test cases.

--- test/com/biomatters/plugins/eupathdb/utils/JSONParserTest.java
+++ test/com/biomatters/plugins/eupathdb/utils/JSONParserTest.java
@@ -1,15 +1,21 @@
 package com.biomatters.plugins.eupathdb.utils;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.biomatters.geneious.publicapi.databaseservice.DatabaseServiceException;
+import com.google.gson.JsonSyntaxException;
 
 /**
  * The class <code>JSONParserTest</code> contains tests for the class
@@ -19,6 +25,10 @@ import org.junit.Test;
  * @version $Revision: 1.0 $
  */
 public class JSONParserTest {
+	String responseJson = "";
+	FileReader reader = null;
+	BufferedReader bufferedReader = null;
+
 	/**
 	 * Run the List<Map<String, String>> parseJSON(String) method test.
 	 * 
@@ -27,29 +37,27 @@ public class JSONParserTest {
 	 */
 	@Test
 	public void testParseJSON() throws Exception {
-		FileReader reader = null;
-		BufferedReader bufferedReader = null;
-		String inputLine;
-		StringBuilder jsonString = new StringBuilder();
-		try {
-			reader = new FileReader(getClass().getResource(
-					"JSONResponseString.txt").getPath());
-			bufferedReader = new BufferedReader(reader);
+		read("JSONResponseString.txt");
+		List<Map<String, String>> result = JSONParser.parse(responseJson);
+		assertEquals("PF3D7_1133400",result.get(0).get("primary_key"));
+	}
 
-			while ((inputLine = bufferedReader.readLine()) != null) {
-				jsonString.append(inputLine);
-			}
-		} finally {
-			if (reader != null) {
-				reader.close();
-			}
-			if (bufferedReader != null) {
-				bufferedReader.close();
-			}
+	@Test
+	public void testParseErrorJSON() throws Exception {
+		read("JSONErrorResponse.txt");
+		List<Map<String, String>> result = JSONParser.parse(responseJson);
+		assertEquals("020", result.get(0).get(EuPathDBConstants.RESPONSE_KEY_ERROR_CODE));
+	}
+
+	@Test
+	public void testParseBrokenJSON() throws Exception {
+		read("JSONBrokenResponse.txt");
+		try {
+			JSONParser.parse(responseJson);
+			fail("It should have thrown DatabaseServiceException.");
+		} catch (DatabaseServiceException e) {
+			assertTrue("It should have thrown JsonSyntaxException.", e.getCause() instanceof JsonSyntaxException);
 		}
-		List<Map<String, String>> result = JSONParser.parse(jsonString
-				.toString());
-		assertNotNull(result);
 	}
 
 	/**
@@ -74,6 +82,12 @@ public class JSONParserTest {
 	@After
 	public void tearDown() throws Exception {
 		// Add additional tear down code here
+		if (reader != null) {
+			reader.close();
+		}
+		if (bufferedReader != null) {
+			bufferedReader.close();
+		}
 	}
 
 	/**
@@ -85,5 +99,21 @@ public class JSONParserTest {
 	 */
 	public static void main(String[] args) {
 		new org.junit.runner.JUnitCore().run(JSONParserTest.class);
+	}
+
+	/**
+	 * @param Filename
+	 * @throws IOException
+	 */
+	private void read(String Filename) throws IOException {
+		String inputLine;
+		StringBuilder jsonString = new StringBuilder();
+		reader = new FileReader(getClass().getResource(Filename).getPath());
+		bufferedReader = new BufferedReader(reader);
+
+		while ((inputLine = bufferedReader.readLine()) != null) {
+			jsonString.append(inputLine);
+		}
+		responseJson = jsonString.toString();
 	}
 }
