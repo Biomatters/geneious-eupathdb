acd2e0128d47805466a44e3c5e63215e72608460
Matthew Cheung <matthew@biomatters.com>
2016-05-02 13:24:11 +1200

    Fix compilation problem introduced by Alex's last change

--- test/com/biomatters/plugins/eupathdb/database/EukaryoticDatabaseTest.java
+++ test/com/biomatters/plugins/eupathdb/database/EukaryoticDatabaseTest.java
@@ -19,7 +19,9 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import javax.ws.rs.core.Response;
