1a55165acec96f17d43d11b4d781d1d86f32d566
Matthew Cheung <matthew@biomatters.com>
2017-02-15 17:19:30 +1300

    GEN-28376 Release EuPathDB 1.0.1
    Move release notes to a separate file

---
+++ release_notes.md
@@ -0,0 +1,7 @@
+# v1.0 (7th October 2015)
+First release of new EuPathDB plugin based on original PiroplasmoDB and PiroplasmaDB plugins by Charles Ma.
+
+Adds to [Geneious](http://geneious.com) the ability to submit searches to all of the EuPathDB pathogen databases.
+
+# v1.0.1 (Date TBD)
+- GEN-28102 Fixed search failures by updating to be compatible with EuPathDB server changes
--- src/com/biomatters/plugins/eupathdb/EuPathDBPlugin.java
+++ src/com/biomatters/plugins/eupathdb/EuPathDBPlugin.java
@@ -90,12 +90,6 @@ public class EuPathDBPlugin extends GeneiousPlugin {
         return PLUGIN_MIN_API_VERSION;
     }
 
-    /**
-     *  1.0 released 2015-09-01
-     *   - first release
-     *  1.0.1 released TBD
-     *   - GEN-28102 Fixed search failures by updating to be compatible with EuPathDB server changes
-     */
     @Override
     public String getVersion() {
         return PLUGIN_VERSION;
